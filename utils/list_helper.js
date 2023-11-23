ad4337ba4f87019b5cd88096a641468c5334509d
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-11-23 11:19:18 +0400

    4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,15 @@ const totalLikes = (blogs) => {
     blogs.reduce((sum, item) => sum + item)
 }
 
+const favoriteBlog = (blogs) => {
+  const blogLikes = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...blogLikes)
+  const favBlog = blogs.filter(blog => blog.likes === maxLikes)
+  return favBlog[0]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
