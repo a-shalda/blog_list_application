acd6a2d444cb1a8cbe6c165a508957e82f9cd2ef
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-12-11 15:50:21 +0400

    changed the order of lines in app

--- app.js
+++ app.js
@@ -24,11 +24,11 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static('dist'))
 app.use('/api/users', usersRouter)
 app.use('/api/blogs/comments', commentsRouter)
 app.use('/api/blogs', blogRouter)
 app.use('/api/login', loginRouter)
-
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
@@ -37,6 +37,5 @@ if (process.env.NODE_ENV === 'test') {
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
-app.use(express.static('dist'))
 
 module.exports = app
