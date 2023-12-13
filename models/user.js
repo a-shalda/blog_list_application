701dac9fc08a052139b5bd5d20888ed1790ce344
Alex Shalda <123298731+a-shalda@users.noreply.github.com>
2023-12-13 10:53:24 +0400

    changed min user length

--- models/user.js
+++ models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    minLength: 3
+    minLength: 1
   },
   name: String,
   passwordHash: String,
